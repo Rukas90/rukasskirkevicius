c1f250c3f912db6d3aeba24290ae9166fbc192e3
Rukas Skirkevicius <rukas.skirkevicius@gmail.com>
2026-03-21 19:24:11 +0200

    Update EducationSection.tsx

--- src/components/sections/EducationSection.tsx
+++ src/components/sections/EducationSection.tsx
@@ -9,17 +9,20 @@ const EducationSection = () => {
   return (
     <SectionContainer>
       <SectionText>{t("education", "Education")}</SectionText>
-      <div className="inline-flex gap-6 items-center">
-        <div className="shine">
+      <div className="inline-flex sm:gap-6 gap-4 items-center">
+        <div className="shine min-w-15 min-h-15">
           <a href="https://www.saxion.edu/" target="_blank">
-            <img src={SaxionLogo} className="size-15 rounded-md" />
+            <img
+              src={SaxionLogo}
+              className="w-15 h-15 rounded-md object-cover"
+            />
           </a>
         </div>
         <div>
           <p className="text-sm font-medium text-stone-800">
             Saxion University of Applied Sciences
           </p>
-          <p className="text-sm text-stone-700">
+          <p className="sm:text-sm text-xs text-stone-700">
             Bachelor's degree, Game and Interactive Media Design
           </p>
           <p className="text-sm font-light text-stone-700">2017 – 2021</p>
